bf3bc7552c2d571f55d0d2892134c4ece3aa661c
Dariusz <darekmakarewicz1994@gmail.com>
2026-08-03 08:26:39 +0200

    clean the files

--- tasks/build_release.ps1
+++ tasks/build_release.ps1
@@ -1,14 +1,8 @@
 uv python install 3.14 ; 
 uv python pin 3.14 ; 
-uv sync --dev --no-cache --locked ; 
-
-.venv\Scripts\Activate.ps1 ; 
-$env:UV_ENV_FILE = ".dev.env" ; 
-
-.\scripts\format_and_lint.ps1 ; 
-
-uv run pytest tests/ --cov=src -vv ;
 uv sync --no-dev --locked --no-cache ; 
+
 uv run pyinstaller --clean .\scripts\standalone_build_windows.spec ; 
-cp -r -fo .\dist\* .\releases\windows\ ; 
-rm -r -fo .\dist, .\build ; 
+
+Copy-Item -r -fo .\dist\* .\releases\windows\ ; 
+Remove-Item -r -fo .\dist, .\build ; 
