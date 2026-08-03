07d0eafb3f14e6b888e0183703f4f007672db655
Dariusz <darekmakarewicz1994@gmail.com>
2026-08-03 08:31:44 +0200

    update files

--- tasks/build_release.ps1
+++ tasks/build_release.ps1
@@ -1,8 +1,8 @@
-uv python install 3.14 ; 
-uv python pin 3.14 ; 
 uv sync --no-dev --locked --no-cache ; 
 
 uv run pyinstaller --clean .\scripts\standalone_build_windows.spec ; 
 
 Copy-Item -r -fo .\dist\* .\releases\windows\ ; 
-Remove-Item -r -fo .\dist, .\build ; 
+Remove-Item -r -fo .\dist, .\build ;
+ 
+uv sync --dev --locked --no-cache ; 
