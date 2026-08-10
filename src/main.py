a31b7090ad2cfdf8a3be99b65705f6d571e83669
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-08-10 20:23:12 +0200

    Revert "use colorama"
    This reverts commit 6ab3ca356def7ae0beb5d23b4b64908965c47241.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import subprocess
+import sys
 import contextlib
 import logging
 import os
@@ -10,7 +12,6 @@ from typing import Any, TypeAlias, cast
 
 import numpy as np
 import pyloudnorm as pyln
-from colorama import just_fix_windows_console
 from numpy.typing import NDArray
 from pydub import AudioSegment
 from scipy.signal import lfilter
@@ -74,8 +75,8 @@ def format_loudness_params(
 
 def clear_console() -> None:
     """Clears the console output completely."""
-    just_fix_windows_console()
-
+    command = "cls" if sys.platform == "win32" else "clear"
+    subprocess.run([command], check=False)
 
 class ConsoleCleaner:
     """Runs a background thread to clear the console at regular intervals."""
