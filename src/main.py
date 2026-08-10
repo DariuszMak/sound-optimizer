2caaa552d7b1079413d48d577452b54421ecafd3
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-08-10 14:51:16 +0200

    use maximum workers

--- src/main.py
+++ src/main.py
@@ -439,7 +439,7 @@ def main() -> None:
         if not tasks:
             return
 
-        workers = max(1, cpu_count() // 2)
+        workers = max(1, cpu_count() // 1)
         slot_counter = Value("i", 0)
         with Pool(workers, initializer=_init_worker, initargs=(slot_counter,)) as pool:
             list(
